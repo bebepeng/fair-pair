93e6aa4be40441a68e956427ebcde4def26c625f
Bebe Peng <bebepeng@gmail.com>
2014-07-02 11:56:28 -0600

    changed pomodoro spec to use less time

--- spec/features/timer_spec.rb
+++ spec/features/timer_spec.rb
@@ -4,12 +4,16 @@ feature 'Pairing Timers' do
   scenario 'User can set a pomodoro', :js => true do
     visit root_path
     click_on 'Use Timers'
+
     click_on 'Set a Pomodoro'
     expect(page).to have_content '25:0'
+
     click_on 'Start'
-    sleep 5
+    sleep 2
     click_on 'Pause'
+    expect(page).to have_content '24:58'
     expect(page).to have_no_content '25:0'
+
     click_on 'Reset'
     expect(page).to have_content '25:0'
   end
