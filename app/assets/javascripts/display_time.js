17d762f3653c3c744ab46e8f664521f3597dbae0
Bebe Peng <bebepeng@gmail.com>
2014-07-10 11:59:42 -0600

    white space

--- app/assets/javascripts/display_time.js
+++ app/assets/javascripts/display_time.js
@@ -1,5 +1,5 @@
-function displayTime (time) {
-  var min = ('0' + Math.floor(time/60000)).slice(-2);
-  var sec = ('0' + Math.floor((time%60000)/1000)).slice(-2);
+function displayTime(time) {
+  var min = ('0' + Math.floor(time / 60000)).slice(-2);
+  var sec = ('0' + Math.floor((time % 60000) / 1000)).slice(-2);
   return min + ":" + sec
 }
