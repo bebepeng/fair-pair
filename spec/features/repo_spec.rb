fcd5b55b83675877b8cefcec0271b486f1faed62
Bebe Peng <bebepeng@gmail.com>
2014-08-05 19:13:12 -0600

    added test for invalid repo

--- spec/features/repo_spec.rb
+++ spec/features/repo_spec.rb
@@ -21,6 +21,27 @@ feature 'GitHub Repo Commit Tracker' do
     end
   end
 
+  scenario 'Users cannot add an invalid repo' do
+    User.create!(:name => 'Sam', :email => 'sam@example.com', :password => 'password')
+
+    visit new_sessions_path
+    fill_in 'Email', :with => 'sam@example.com'
+    fill_in 'Password', :with => 'password'
+    click_button 'Sign In'
+    VCR.use_cassette('githubapi/commits/featurefail') do
+      click_on 'GitHub Repos'
+      within '.form' do
+        fill_in 'GitHub Repo', :with => 'bebepeng/blahstuff'
+        click_on 'Add Repo'
+      end
+
+      expect(page).to have_content 'Invalid Repo'
+      within '#repos-div' do
+        expect(page).to have_no_content 'bebepeng/blahstuff'
+      end
+    end
+  end
+
   scenario 'Users must log in to use GitHub Repo Commit Tracker' do
     visit root_path
     click_on 'GitHub Repos'
