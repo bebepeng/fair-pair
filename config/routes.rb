97e114c719107d702ccd7a4c12479eaa00d0539d
Bebe Peng <bebepeng@gmail.com>
2014-08-06 14:39:01 -0600

    added about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get 'about' => 'welcome#about'
   get 'timers' => 'timers#index'
   resources :users, :only => [:new, :create]
   resource :sessions, :only => [:new, :create, :destroy]
